b876f24b3e8603d1609b71deaef35ef460f81203
SharpDevOps10 <td090833@gmail.com>
2024-02-25 19:19:40 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SharpDevOps10/Methodologies-Lab1
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+func convertMarkdownToHTML(markdown string) {
+    // TODO: implement the function that converts markdown to HTML
+}
+
+func main() {
+
+}
